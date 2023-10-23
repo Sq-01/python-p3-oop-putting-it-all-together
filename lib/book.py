3549a073ae40399e0f8a60e4760f6fdc9a0c97cb
Sadiq <sadiq.said@student.moringaschool.com>
2023-10-23 21:52:45 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+# Run the tests
+if __name__ == "__main__":
+    book = Book("And Then There Were None", 272)
+    assert book.page_count == 272
+    assert book.title == "And Then There Were None"
+
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    book.page_count = "not an integer"
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "page_count must be an integer\n"
+
+    book = Book("The World According to Garp", 69)
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    book.turn_page()
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "Flipping the page...wow, you read fast!\n"
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,43 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+        else:
+            self._size = value
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
+
+# Run the tests
+if __name__ == "__main__":
+    stan_smith = Shoe("Adidas", 9)
+    assert stan_smith.brand == "Adidas"
+    assert stan_smith.size == 9
+
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    stan_smith.size = "not an integer"
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "size must be an integer\n"
+
+    captured_out = io.StringIO()
+    sys.stdout = captured_out
+    stan_smith.cobble()
+    sys.stdout = sys.__stdout__
+    assert captured_out.getvalue() == "Your shoe is as good as new!\n"
+
+    stan_smith = Shoe("Adidas", 9)
+    stan_smith.cobble()
+    assert stan_smith.condition == "New"
